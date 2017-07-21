cabf1402f178bc89111bd99b909db77b638d21ae
Dan Pelensky <dan@pelensky.com>
2017-07-21 13:58:31 +0100

    initial commit

---
+++ spec/game_of_life/core_spec.clj
@@ -0,0 +1,7 @@
+(ns game-of-life.core-spec
+  (:require [speclj.core :refer :all]
+            [game-of-life.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/game_of_life/core.clj
@@ -0,0 +1,5 @@
+(ns game-of-life.core)
+
+(defn -main
+  [& args]
+  (println "Hello World"))
