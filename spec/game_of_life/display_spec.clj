a9c550b4042e327bf464c0d1eba271843db0ac16
Dan Pelensky <dan@pelensky.com>
2017-07-27 14:31:21 +0100

    prints a world with a cell in it

--- spec/game_of_life/display_spec.clj
+++ spec/game_of_life/display_spec.clj
@@ -5,4 +5,7 @@
 (describe "Displays"
           (it "an empty grid when passed an empty grid"
               (should-contain "          \n          \n          \n         "
-                              (with-out-str (display ["          " "          " "          " "          "])))))
+                              (with-out-str (display ["          " "          " "          " "          "]))))
+          (it "a grid with some life in it"
+              (should-contain "*         \n          \n          "
+                              (with-out-str (display ["*         " "          " "          "])))))
--- src/game_of_life/display.clj
+++ src/game_of_life/display.clj
@@ -1,9 +1,5 @@
 (ns game-of-life.display)
 
 (defn display [grid]
-  (println "          ")
-  (println "          ")
-  (println "          ")
-  (println "          ")
-  (println "          "))
+  (println (clojure.string/join "\n" grid)))
 
