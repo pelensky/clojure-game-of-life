9cfea7d9d917e47c76eefb089221f0c7aa96a0d2
Dan Pelensky <dan@pelensky.com>
2017-07-27 17:54:44 +0100

    removed unnessary dos

--- src/game_of_life/display.clj
+++ src/game_of_life/display.clj
@@ -3,18 +3,16 @@
 (defn format-rows [coordinates grid x y]
   (if (empty? coordinates)
     grid
-    (do 
-      (let [highest-x (max x (apply max (map #(get % 0) coordinates)))
-            highest-y (max y (apply max (map #(get % 1) coordinates)))]
-        (if (= grid [])
-          (recur coordinates (into [] (conj (repeat (inc highest-y) (into [] (repeat (inc highest-x) " " ))))) highest-x highest-y )
-          (do
-            (let [current-coordinate (first coordinates)
-                  updated-coordinates (into [] (rest coordinates))
-                  current-x (get current-coordinate 0)
-                  current-y (get current-coordinate 1)
-                  updated-grid (assoc-in grid [current-y current-x] "*")]
-            (recur updated-coordinates updated-grid x y))))))))
+    (let [highest-x (max x (apply max (map #(get % 0) coordinates)))
+          highest-y (max y (apply max (map #(get % 1) coordinates)))]
+      (if (= grid [])
+        (recur coordinates (into [] (conj (repeat (inc highest-y) (into [] (repeat (inc highest-x) " " ))))) highest-x highest-y )
+        (let [current-coordinate (first coordinates)
+              updated-coordinates (into [] (rest coordinates))
+              current-x (get current-coordinate 0)
+              current-y (get current-coordinate 1)
+              updated-grid (assoc-in grid [current-y current-x] "*")]
+          (recur updated-coordinates updated-grid x y)) ))))
 
 (defn display [coordinates]
   (format-rows coordinates [] 0 0 ))
