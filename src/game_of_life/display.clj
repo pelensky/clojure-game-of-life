f3f8db56c5ed90f9e5a311f329278b29c49e86d7
Dan Pelensky <dan@pelensky.com>
2017-07-27 17:58:45 +0100

    check if grid is empty rather than compare it to an empty grid

--- src/game_of_life/display.clj
+++ src/game_of_life/display.clj
@@ -4,9 +4,10 @@
   (if (empty? coordinates)
     grid
     (let [highest-x (max x (apply max (map #(get % 0) coordinates)))
-          highest-y (max y (apply max (map #(get % 1) coordinates)))]
-      (if (= grid [])
-        (recur coordinates (into [] (conj (repeat (inc highest-y) (into [] (repeat (inc highest-x) " " ))))) highest-x highest-y )
+          highest-y (max y (apply max (map #(get % 1) coordinates)))
+          updated-grid (into grid (conj (repeat (inc highest-y) (into [] (repeat (inc highest-x) " " )))))]
+      (if (empty? grid)
+        (recur coordinates updated-grid highest-x highest-y )
         (let [current-coordinate (first coordinates)
               updated-coordinates (into [] (rest coordinates))
               current-x (get current-coordinate 0)
