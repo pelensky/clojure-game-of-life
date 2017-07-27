6217946c2a6573e077fa8fa7e425602cc952eaaa
Dan Pelensky <dan@pelensky.com>
2017-07-27 15:37:33 +0100

    gets the highest coordinates for x and y

--- spec/game_of_life/display_spec.clj
+++ spec/game_of_life/display_spec.clj
@@ -3,9 +3,6 @@
             [game-of-life.display :refer :all]))
 
 (describe "Displays"
-          (it "an empty grid when passed an empty grid"
-              (should-contain "          \n          \n          \n         "
-                              (with-out-str (display ["          " "          " "          " "          "]))))
-          (it "a grid with some life in it"
-              (should-contain "*         \n          \n          "
-                              (with-out-str (display ["*         " "          " "          "])))))
+          (it "only the cell if it is in position 0 1"
+              (should-contain "1010"
+                              (with-out-str (display [[10 0] [0 10]])))))
--- src/game_of_life/display.clj
+++ src/game_of_life/display.clj
@@ -1,5 +1,10 @@
 (ns game-of-life.display)
 
-(defn display [grid]
-  (println (clojure.string/join "\n" grid)))
+(defn format-grid [coordinates grid x y]
+  (let [highest-x (max x (apply max (map #(get % 0) coordinates)))
+        highest-y (max y (apply max (map #(get % 1) coordinates)))]
+      (println (str  highest-x highest-y))))
+
+(defn display [coordinates]
+  (println (format-grid coordinates [] 0 0)))
 
