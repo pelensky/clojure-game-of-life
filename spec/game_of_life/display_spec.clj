14e028982526c712007a08d7199ec5ea9e3ae263
Dan Pelensky <dan@pelensky.com>
2017-07-27 17:34:11 +0100

    additional test

--- spec/game_of_life/display_spec.clj
+++ spec/game_of_life/display_spec.clj
@@ -13,4 +13,12 @@
 
           (it "returns two cells if given 2"
               (should= [["*"] ["*"]]
-                (format-rows [[0 0] [0 1]] [] 0 0))))
+                (format-rows [[0 0] [0 1]] [] 0 0)))
+
+          (it "returns a cell not in position zero"
+              (should= [[" " " "] [" " "*"]]
+                       (format-rows [[1 1]] [] 0 0)))
+
+          (it "returns rows for a high number"
+              (should= [[" " " " " " " " " "][" " " " " " " " " "] [" " " " " " " " " "] [" " " " " " " " " "] [" " " " " " " " " "] [" " " " " " " " " "] [" " " " " " " " " "] [" " " " " " " " " "] [" " " " " " " " " "] [" " " " " " " " "*"]]
+                      (format-rows [[4 9]] [] 0 0) )))
--- src/game_of_life/display.clj
+++ src/game_of_life/display.clj
@@ -4,18 +4,17 @@
   (if (empty? coordinates)
     grid
     (do 
-  (let [highest-x (max x (apply max (map #(get % 0) coordinates)))
-        highest-y (max y (apply max (map #(get % 1) coordinates)))]
-      (if (= grid [])
-        (recur coordinates (into [] (conj (repeat (inc highest-y) (into [] (repeat (inc highest-x) " " ))))) highest-x highest-y )
-        (do
+      (let [highest-x (max x (apply max (map #(get % 0) coordinates)))
+            highest-y (max y (apply max (map #(get % 1) coordinates)))]
+        (if (= grid [])
+          (recur coordinates (into [] (conj (repeat (inc highest-y) (into [] (repeat (inc highest-x) " " ))))) highest-x highest-y )
+          (do
             (let [current-coordinate (first coordinates)
                   updated-coordinates (into [] (rest coordinates))
                   current-x (get current-coordinate 0)
                   current-y (get current-coordinate 1)
                   updated-grid (assoc-in grid [current-y current-x] "*")]
-            (recur updated-coordinates updated-grid x y)
-              )))))))
+            (recur updated-coordinates updated-grid x y))))))))
 
 (defn display [coordinates]
   (format-rows coordinates [] 0 0 ))
