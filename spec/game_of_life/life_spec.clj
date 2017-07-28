40b3920322a4ba0ac7695a69db63d2f35846b603
Dan Pelensky <dan@pelensky.com>
2017-07-28 14:14:25 +0100

    count all neighbours function created

--- spec/game_of_life/life_spec.clj
+++ spec/game_of_life/life_spec.clj
@@ -6,4 +6,23 @@
 (describe "Life"
           (it "An empty grid stays empty"
               (should= []
-                       (evolve []))))
+                       (evolve [])))
+
+          (it "A single living cell can not support life"
+              (should= []
+                      (evolve [[0 0]])))
+
+          (it "A live cell with 2 live neighbours lives on"
+              (should= true
+                    (live-on? [1 0] [[0 0] [1 0] [2 0]])))
+
+          (it "A cell with 3 live neighbours lives on"
+              (should= true
+                       (live-on? [0 0] [[-1 0] [0 0] [-1 -1] [0 -1]])))
+
+          (it "Finds all neighbours of a cell"
+              (should= [[-1 1] [0 1] [1 1] [-1 0] [1 0] [-1 -1] [0 -1] [1 -1] ]
+                       (find-all-neighbours [0 0])))
+          (it "Counts the live neighbours of a cell"
+              (should= 0
+                       (count-live-neighbours [0 0] [[0 0]]))))
--- src/game_of_life/life.clj
+++ src/game_of_life/life.clj
@@ -1,4 +1,21 @@
 (ns game-of-life.life)
 
+(def neighbours
+  [ [-1 1]  [0 1]   [1 1]
+    [-1 0]          [1 0]
+    [-1 -1] [0 -1]  [1 -1]])
+
+(defn find-all-neighbours [cell]
+   (let [x (get cell 0)
+         y (get cell 1)]
+      map #([(+ x (get % 0)) (+ y (get % 1))]) neighbours))
+
+(defn count-live-neighbours [cell grid]
+   (let [neighbours (find-all-neighbours cell)]
+     0))
+
+(defn live-on? [cell grid]
+   true)
+
 (defn evolve [grid]
-  [])
+  [] )
